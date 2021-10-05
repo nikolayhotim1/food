efbadd9dd329f2ee5033b2d5dde7550b9d6560da
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-10-05 21:11:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+(function (food) {
+    if (food === 'cookies') {
+        alert('More please');
+    } else if (food === 'cake') {
+        alert('Yum yum');
+    }
+})('cookies'); // More please
+
+(function (food) {
+    if (food === 'cookies') {
+        alert('More please');
+    } else if (food === 'cake') {
+        alert('Yum yum');
+    }
+})('cake'); // Yum yum
+
+// function say(food) {
+//     if (food === 'cookies') {
+//         alert('More please');
+//     } else if (food === 'cake') {
+//         alert('Yum yum');
+//     }
+// }
+
+// say('cookies');
+// say('cake');
+
+// let say = function (food) {
+//     if (food === 'cookies') {
+//         alert('More please');
+//     } else if (food === 'cake') {
+//         alert('Yum yum');
+//     }
+// }
+
+// say('cookies');
+// say('cake');
